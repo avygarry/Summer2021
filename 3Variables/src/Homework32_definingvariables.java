ec89a2eaa914e92509e2958e396f7d63cf8261ca
Avy <avy.garry@gmail.com>
2021-06-23 22:53:00 -0400

    More changes

---
+++ 3Variables/src/Homework32_definingvariables.java
@@ -0,0 +1,15 @@
+/**
+ * Chapter 3 Homework 
+ * For this homework I have to define a constant for Pi and define variables as per variable naming convention
+ * @author Avy
+ *
+ */
+public class Homework32_definingvariables {
+	public static void main(String[] args) {
+		float PI = 3.14f;
+		boolean accuracy = true;
+			
+		
+	}
+
+}
