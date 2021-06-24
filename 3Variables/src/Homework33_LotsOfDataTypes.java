06706d630a110697fdd841958bc6f10748e4cc55
Avy <avy.garry@gmail.com>
2021-06-23 23:29:00 -0400

    Update

--- 3Variables/src/Homework33_LotsOfDataTypes.java
+++ 3Variables/src/Homework33_LotsOfDataTypes.java
@@ -1,37 +1,37 @@
 /**
  * Chapter 3 Homework 3
- * 
+ * For this homework I have to add a bunch of variables and print them.
  * @author Avy
  */
 public class Homework33_LotsOfDataTypes {
 	public static void main(String[] args) {
-		byte LowByte = -128;
-		byte HighByte = 127;
-		short LowShort = -32768;
-		short HighShort = 32767;
-		int LowInt = -2147483648;
-		int HighInt = 2147483647;
-		long LowLong = -9223372036854775808l;
-		long HighLong = 9223372036854775807l;
-		float LowFloat = Float.MIN_VALUE;
-		float HighFloat = Float.MAX_VALUE;
-		double LowDouble = Double.MIN_VALUE;
-		double HighDouble = Double.MAX_VALUE;
-		boolean LowBoo = false;
-		boolean HighBoo = true;
-		System.out.println("The lowest Byte is " + LowByte);
-		System.out.println("The highest Byte is " + HighByte);
-		System.out.println("The lowest Short is " + LowShort);
-		System.out.println("The highest Short is " + HighShort);
-		System.out.println("The lowest Int is " + LowInt);
-		System.out.println("The highest Int is " + HighInt);
-		System.out.println("The lowest Long is " + LowLong);
-		System.out.println("The highest Long is " + HighLong);
-		System.out.println("The lowest Float is " + LowFloat);
-		System.out.println("The highest Float is " + HighFloat);
-		System.out.println("The lowest Double is " + LowDouble);
-		System.out.println("The highest Doublee is " + HighDouble);
-		System.out.println("The lowest Boolean is " + LowBoo);
-		System.out.println("The highest Boolean is " + HighBoo);
+		byte lowByte = -128;
+		byte highByte = 127;
+		short lowShort = -32768;
+		short highShort = 32767;
+		int lowInt = -2147483648;
+		int highInt = 2147483647;
+		long lowLong = -9223372036854775808l;
+		long highLong = 9223372036854775807l;
+		float lowFloat = Float.MIN_VALUE;
+		float highFloat = Float.MAX_VALUE;
+		double lowDouble = Double.MIN_VALUE;
+		double highDouble = Double.MAX_VALUE;
+		boolean lowBoo = false;
+		boolean highBoo = true;
+		System.out.println("The lowest Byte is " + lowByte);
+		System.out.println("The highest Byte is " + highByte);
+		System.out.println("The lowest Short is " + lowShort);
+		System.out.println("The highest Short is " + highShort);
+		System.out.println("The lowest Int is " + lowInt);
+		System.out.println("The highest Int is " + highInt);
+		System.out.println("The lowest Long is " + lowLong);
+		System.out.println("The highest Long is " + highLong);
+		System.out.println("The lowest Float is " + lowFloat);
+		System.out.println("The highest Float is " + highFloat);
+		System.out.println("The lowest Double is " + lowDouble);
+		System.out.println("The highest Double is " + highDouble);
+		System.out.println("The lowest Boolean is " + lowBoo);
+		System.out.println("The highest Boolean is " + highBoo);
 	}
 }
