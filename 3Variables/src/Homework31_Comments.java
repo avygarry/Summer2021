f6dca5854bd8d473483e18818476a3f5d391f30b
Avy <avy.garry@gmail.com>
2021-06-23 22:51:51 -0400

    Finished more stuff

---
+++ 3Variables/src/Homework31_Comments.java
@@ -0,0 +1,16 @@
+/**
+ * Chapter 3 Homework 1
+ * This shows 3 different types of comments
+ * the slash double star is used to enter java doc.
+ * @author Avy
+ */
+public class Homework31_Comments {
+	public static void main(String[] args) {
+	//This is a single line command
+	/* This
+	 * Is a multi line
+	 * Comment         */
+	
+	
+	}
+}
