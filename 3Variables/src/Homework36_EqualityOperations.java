2924fd189815795c09fff380e32fec9ff56f8917
Avy <avy.garry@gmail.com>
2021-06-24 02:34:22 -0400

    Chapter 3 Homework 6 done

---
+++ 3Variables/src/Homework36_EqualityOperations.java
@@ -0,0 +1,32 @@
+/**
+ * Chapter 3 Homework 6
+ * For this homework I have to create multiple variables and print them if they are greater than, less than or equal to
+ * @author Avy
+ */
+public class Homework36_EqualityOperations {
+	public static void main(String[] args) {
+		int num1G = 15;
+		int num2G = 13;
+			if (num1G > num2G) {
+				System.out.println(num1G + " is greater than " + num2G);
+			}
+		int num1L = 29;
+		int num2L = -30;
+			if (num1L < num2L) {
+			System.out.println(num1L + " is less than " + num2L);
+		}
+		int num1E = 598;
+		int num2E = 598;
+			if (num1E == num2E) {
+			System.out.println(num1E + " is equal to " + num2E);
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
