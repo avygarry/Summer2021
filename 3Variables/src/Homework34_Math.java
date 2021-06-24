b23058535dcfd3a868e07279db3101a7c27ccf3b
Avy <avy.garry@gmail.com>
2021-06-24 01:54:04 -0400

    Finished Chapter 3 Homework 4

--- 3Variables/src/Homework34_Math.java
+++ 3Variables/src/Homework34_Math.java
@@ -1,4 +1,42 @@
-
+/**
+ * Chapter 3 Homework 4
+ * For this homework I have to take 2 numbers for each type of operation and do that operation
+ * @author Avy
+ */
 public class Homework34_Math {
-
+	public static void main(String[] args) {
+		int num1Add2 = 9;
+		int num2Add2 = 10;
+		int finalAdd2 = num1Add2 + num2Add2;
+		System.out.println(num1Add2 + " + " + num2Add2 + " = " + finalAdd2);
+		int num1Subtract = 56;
+		int num2Subtract = 27;
+		int finalSubtract = num1Subtract - num2Subtract;
+		System.out.println(num1Subtract + " - " + num2Subtract + " = " + finalSubtract);
+		int num1Multiply = 7;
+		int num2Multiply = 6;
+		int finalMultiply = num1Multiply * num2Multiply;
+		System.out.println(num1Multiply + " x " + num2Multiply + " = " + finalMultiply);
+		int num1Divide = 62;
+		int num2Divide = 4;
+		int finalDivide = num1Divide / num2Divide;
+		System.out.println(num1Divide + " ÷ " + num2Divide + " = " + finalDivide);
+		int num1Remain = 10;
+		int num2Remain = 3;
+		int finalRemain = num1Remain % num2Remain;
+		System.out.println(num1Remain + " divided by " + num2Remain + " creates a remainder of " + finalRemain);
+		int num1Add3 = 29;
+		int num2Add3 = 56;
+		int num3Add3 = 82;
+		int finalAdd3 = num1Add3 + num2Add3 + num3Add3;
+		System.out.println(num1Add3 + " + " + num2Add3 + " + " + num3Add3 + " = " + finalAdd3);
+		int num1AdM3 = 27;
+		int num2AdM3 = 15;
+		int num3AdM3 = 3;
+		int finalAdM3 = num1AdM3 + num2AdM3;
+		int final2AdM3 = finalAdM3 * 3;
+		System.out.println("(" + num1AdM3 + " + " + num2AdM3 + ") x " + num3AdM3 + " = " + final2AdM3);
+		
+		
+	}
 }
