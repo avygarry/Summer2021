1c1fa7611456c91e9f8ff38dd85c69a03665d112
Avy <avy.garry@gmail.com>
2021-06-26 16:43:37 -0400

    no message

---
+++ 3Variables/src/Homework37_VariableSwap.java
@@ -0,0 +1,20 @@
+/**
+ * Chapter 3 Homework 7
+ * This program will swap 2 variables
+ * @author Avy
+ */
+public class Homework37_VariableSwap {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 0;
+		num3 = num1;
+		num1 = num2;
+		num2 = num3;
+		num3 = 0;
+		System.out.println(num1);
+		System.out.println(num2);
+		
+	}
+
+}
