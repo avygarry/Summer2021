0c01ddc8ca3026e2a9a7bde1d90533ff25edc49d
Avy <avy.garry@gmail.com>
2021-06-26 13:05:25 -0400

    no message

---
+++ 3Variables/src/CondtionalOperator.java
@@ -0,0 +1,23 @@
+/**
+ * This program shows Conditional Operations
+ * @author Avy
+ *
+ */
+public class CondtionalOperator {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		
+		int max = 0;
+		
+		if(num1 > num2) {
+			max = num1;
+		}else {
+			max = num2;
+		}
+		
+		max = (num1 > num2) ? num1: num2;
+	}
+
+}
