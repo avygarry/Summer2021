7e88b9c43b257bb428909a337724d4320c402682
Avy <avy.garry@gmail.com>
2021-06-26 12:23:05 -0400

    no message

---
+++ 3Variables/src/UnaryOperators.java
@@ -0,0 +1,13 @@
+
+public class UnaryOperators {
+	public static void main(String[] args) {
+		int number1 = 10;
+		number++;
+		System.out.println(number);
+		
+		boolean bigValue = true;
+		boolean littleValue = !bigValue;
+		System.out.println(littleValue);
+	}
+
+}
