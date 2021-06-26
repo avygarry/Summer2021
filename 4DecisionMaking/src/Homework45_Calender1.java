f873d176c484098ecb8e5b6fcaa7f7726f12d3ba
Avy <avy.garry@gmail.com>
2021-06-26 12:46:04 -0400

    no message

---
+++ 4DecisionMaking/src/Homework45_Calender1.java
@@ -0,0 +1,16 @@
+import Java.util.Scanner;
+
+/**
+ * Chapter 4 Homework 5
+ * 
+ * @author Avy
+ */
+public class Homework45_Calender1 {
+	
+	public static int readUserIntInput (String aInputMsg) {
+		System.out.print( aInputMsg + " ");
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextInt();
+	}
+	
+}
