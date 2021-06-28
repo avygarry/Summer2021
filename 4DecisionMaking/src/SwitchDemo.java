b57540b580ca7b04712c8c409a2c0709b46654b0
Avy <avy.garry@gmail.com>
2021-06-28 19:10:39 -0400

    no message

---
+++ 4DecisionMaking/src/SwitchDemo.java
@@ -0,0 +1,23 @@
+
+public class SwitchDemo {
+	public static void main(String[] args) {
+		int num = 3;
+		
+		switch (num) {
+		  case 1: System.out.println("January");
+		  break;
+		  case 2: System.out.println("February");
+		  break;
+		  case 3: System.out.println("March");
+		  break;
+		  case 4: System.out.println("April");
+		  break;
+		  default: System.out.println("Stop");
+		  
+		  
+		
+		
+		}
+	}
+
+}
