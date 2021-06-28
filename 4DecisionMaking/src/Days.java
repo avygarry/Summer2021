a3161fd922ebd67de208fd80ece78624c5bc7921
Avy <avy.garry@gmail.com>
2021-06-28 18:46:51 -0400

    no message

---
+++ 4DecisionMaking/src/Days.java
@@ -0,0 +1,21 @@
+
+public class Days {
+
+	public static void main(String[] args) {
+		int num = 24;
+		if (num == 1) {
+			System.out.println("Sunday");
+		}else if (num == 2) {
+			System.out.println("Monday");
+		}else if (num == 3) {
+			System.out.println("Tuesday");
+		}else if (num == 4) {
+			System.out.println("Wednesday");
+		}else {
+			System.out.println(num +" isn't a valid number.");
+				
+		}
+
+	}
+
+}
---
+++ 4DecisionMaking/src/EvenNumber.java
@@ -0,0 +1,12 @@
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		int num = 20;
+		if (num % 2 == 0) {
+			System.out.println(num + " is an even number.");
+		} else {
+			System.out.println(num + " is an odd number.");
+		}
+	}
+}
