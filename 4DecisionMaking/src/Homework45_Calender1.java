4fa44bf389e96b9859e99261bf8a3941084876c6
Avy <avy.garry@gmail.com>
2021-06-28 19:15:17 -0400

    no message

--- 4DecisionMaking/src/Homework45_Calender1.java
+++ 4DecisionMaking/src/Homework45_Calender1.java
@@ -11,6 +11,28 @@ public class Homework45_Calender1 {
 		System.out.print( aInputMsg + " ");
 		Scanner scanner = new Scanner(System.in);
 		return scanner.nextInt();
+		
+		int month = readUserIntInput ("Enter the month number: ");
+		switch (month) {
+		  case 1: System.out.println("January");
+			  break;
+		case 2: System.out.println("January");
+		break;
+		case 3: System.out.println("January");
+		break;
+		case 4: System.out.println("January");
+		break;
+		case 5: System.out.println("January");
+		case 6: System.out.println("January");
+		case 1: System.out.println("January");
+		case 1: System.out.println("January");
+		case 1: System.out.println("January");
+		case 1: System.out.println("January");
+		case 1: System.out.println("January");
+		case 1: System.out.println("January");
+		
+		}
+		
 	}
 	
 }
