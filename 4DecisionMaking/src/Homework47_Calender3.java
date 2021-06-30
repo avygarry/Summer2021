a2a2f53d9805175814bb50974cb2bbf4eed208e6
Avy <avy.garry@gmail.com>
2021-06-30 01:49:00 -0400

    no message

--- 4DecisionMaking/src/Homework47_Calender3.java
+++ 4DecisionMaking/src/Homework47_Calender3.java
@@ -9,9 +9,66 @@ public class Homework47_Calender3 {
 	}
 	public static void main(String[] args) {
 		
-	
 	int month = readUserIntInput ("Enter the month: ");
-	int year = readUserIntInput ("Enter the month: ");
-	
+	int year = readUserIntInput ("Enter the year: ");
+	int dayInMonth;
+	if (year % 4 == 0 && year % 100 != 0) {
+		switch (month) {
+		  case 1: dayInMonth = 31;
+		break;
+		  case 2: dayInMonth = 29;
+		break;
+		  case 3: dayInMonth = 31;
+		break;
+		  case 4: dayInMonth = 30;
+		break;
+		  case 5: dayInMonth = 31;
+		break;
+		  case 6: dayInMonth = 30;
+		break;
+		  case 7: dayInMonth = 31;
+		break;
+		  case 8: dayInMonth = 31;
+		break;
+		  case 9: dayInMonth = 30;
+		break;
+		  case 10: dayInMonth = 31;
+		break;
+		  case 11: dayInMonth = 30;
+		break;
+		  case 12:dayInMonth = 31;
+		break;
+		}
+		System.out.println(month + ", " + year + " -> " + );
+		}else {
+			switch (month) {
+			  case 1: System.out.println("31");
+			break;
+			  case 2: System.out.println("28");
+			break;
+			  case 3: System.out.println("31");
+			break;
+			  case 4: System.out.println("30");
+			break;
+			  case 5: System.out.println("31");
+			break;
+			  case 6: System.out.println("30");
+			break;
+			  case 7: System.out.println("31");
+			break;
+			  case 8: System.out.println("31");
+			break;
+			  case 9: System.out.println("30");
+			break;
+			  case 10: System.out.println("31");
+			break;
+			  case 11: System.out.println("30");
+			break;
+			  case 12: System.out.println("31");
+			break;
+			}
+		
+			
+			}
+		}
 	}
-}
