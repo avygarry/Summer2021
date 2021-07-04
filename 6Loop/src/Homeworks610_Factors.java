1f8828de8e74ff3b4bb634041eb872a1f91f3b95
Avy <avy.garry@gmail.com>
2021-07-04 18:10:44 -0400

    no message

--- 6Loop/src/Homeworks610_Factors.java
+++ 6Loop/src/Homeworks610_Factors.java
@@ -1,11 +1,19 @@
 /**
- * 
- * 
+ * Chapter 6 Homework 10
+ * This program will tell u the factors for the number the user tells
  * @author Avy
  */
 public class Homeworks610_Factors {
 	public static void main(String[] args) {
+		int num = ReadUserInput.readUserIntInput("Enter a number: ");
 		
+		System.out.print("Factors for " + num + ": ");
+		for (int x = 1; x < num; x++) {
+			if (num % x == 0) {
+				System.out.print(x + ", ");
+			}
+		}
+		System.out.print(num);
 	}
 
 }
