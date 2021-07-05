3a81a4d6ba63dc65de5e95b45e5af8e04c182158
Avy <avy.garry@gmail.com>
2021-07-05 00:28:56 -0400

    no message

---
+++ 6Loop/src/Homeworks611_Prime.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @author Avy
+ *
+ */
+public class Homeworks611_Prime {
+public static void main(String[] args) {
+	int num = ReadUserInput.readUserIntInput("Enter a number: ");
+	int NP = 1;
+	
+	for (int x = 1; x < num; x++) {
+		if (num % x != 0) {
+			NP = 1;
+		}
+		if (num % x != 0) {
+			NP = 2;
+		} 
+	}
+	if (NP == 1) {
+		System.out.println("The entered number is prime");
+	} else {
+		System.out.println("The entered number is not prime");
+	}
+}
+}
