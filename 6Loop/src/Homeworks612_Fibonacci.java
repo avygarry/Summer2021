d9476edcb23f04191cda1233719643957bfaa350
Avy <avy.garry@gmail.com>
2021-07-05 17:51:16 -0400

    no message

--- 6Loop/src/Homeworks612_Fibonacci.java
+++ 6Loop/src/Homeworks612_Fibonacci.java
@@ -1,4 +1,21 @@
-
+/**
+ * Chapter 6 Homework 12 This program will print 10 Fibonacci numbers
+ * 
+ * @author Avy
+ */
 public class Homeworks612_Fibonacci {
-
-}
+	public static void main(String[] args) {
+		int num = ReadUserInput.readUserIntInput("Enter how many numbers you want to print: ");
+		int s1 = 0;
+		int s2 = 1;
+		int timesPrinted = 0;
+		do {
+		System.out.println(s1);
+		System.out.println(s2);
+			s1 = s1 + s2;
+			s2 = s2 + s1;
+			timesPrinted++;
+			timesPrinted++;
+			} while (timesPrinted < num);
+		}
+	}
