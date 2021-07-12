dca53a4130d7d1862e0e4cd7dc958359828e5605
Avy <avy.garry@gmail.com>
2021-07-12 17:22:24 -0400

    no message

--- BankProject/src/AccountActivity.java
+++ BankProject/src/AccountActivity.java
@@ -1,4 +1,39 @@
-
+import java.util.Date;
 public class AccountActivity {
-
+  private Date txnDate;
+  private float amount;
+  private String action; //Credit/Debit
+  private String description;
+  private float runningBalance;
+public Date getTxnDate() {
+	return txnDate;
+}
+public void setTxnDate(Date txnDate) {
+	this.txnDate = txnDate;
+}
+public float getAmount() {
+	return amount;
+}
+public void setAmount(float amount) {
+	this.amount = amount;
+}
+public String getAction() {
+	return action;
+}
+public void setAction(String action) {
+	this.action = action;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public float getRunningBalance() {
+	return runningBalance;
+}
+public void setRunningBalance(float runningBalance) {
+	this.runningBalance = runningBalance;
+}
+  
 }
