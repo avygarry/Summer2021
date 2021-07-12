aa9939e1a39ad5cb67d5113554cc238ee1a90944
Avy <avy.garry@gmail.com>
2021-07-12 17:37:26 -0400

    no message

--- BankProject/src/BankApp.java
+++ BankProject/src/BankApp.java
@@ -2,28 +2,30 @@
 public class BankApp {
 	public static void main(String[] args) {
 		AccountClass avy = new AccountClass();
-		avy.accountNumber = 21;
+		avy.accountNumber = 0;
 		avy.name = "Avy";
 		avy.balance = 20032.34;
 		avy.accountType = 'g';
 		
 		AccountClass karthik = new AccountClass();
-		karthik.accountNumber = 21;
-		karthik.name = "Avy";
+		karthik.accountNumber = 1;
+		karthik.name = "Karthik";
 		karthik.balance = 20032.34;
 		karthik.accountType = 'g';
 		
 		AccountClass dhyeya = new AccountClass();
-		dhyeya.accountNumber = 21;
-		dhyeya.name = "Avy";
+		dhyeya.accountNumber = 2;
+		dhyeya.name = "Dhyeya";
 		dhyeya.balance = 20032.34;
 		dhyeya.accountType = 'g';
 		
 		AccountClass dhruv = new AccountClass();
-		dhruv.accountNumber = 21;
-		dhruv.name = "Avy";
+		dhruv.accountNumber = 3;
+		dhruv.name = "Dhruv";
 		dhruv.balance = 20032.34;
 		dhruv.accountType = 'g';
+		
+		
 		
 		
 }
