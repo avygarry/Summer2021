cc01b4c9deaac7e540538e4c57a7f737e54ea92c
Avy <avy.garry@gmail.com>
2021-07-14 15:30:37 -0400

    no message

---
+++ 8Constructor/src/Homework82_Switch.java
@@ -0,0 +1,18 @@
+
+public class Homework82_Switch {
+	public static void main(String[] args) {
+		boolean switchOn = true;
+		boolean switchOff = false;
+		boolean lampOn = true;
+
+		if (switchOn = true) {
+			switchOff = false;
+			lampOn = true;
+		} else if (switchOff = true) {
+			switchOn = false;
+			lampOn = false;
+		}
+
+	}
+
+}
