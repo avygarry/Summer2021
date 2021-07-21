b4607f35c17bbfdd0d3bc9e841f175d769af7ea2
Avy <avy.garry@gmail.com>
2021-07-20 23:16:28 -0400

    no message

--- 8Constructor/src/Homework83_Planets.java
+++ 8Constructor/src/Homework83_Planets.java
@@ -1,49 +1,47 @@
 
 public class Homework83_Planets {
 	
-	public int orbitalDistance;
-	public int distanceFromSun;
-	public int surfaceArea;
-	public int lengthOfDay;
-	public int radius;
+	public long orbitalDistance;
+	public long distanceFromSun;
+	public long surfaceArea;
+	public long lengthOfDay;
+	public long radius;
 	public boolean hasRings;
-	public int getOrbitalDistance() {
+	public long getOrbitalDistance() {
 		return orbitalDistance;
 	}
-	public void setOrbitalDistance(int orbitalDistance) {
+	public void setOrbitalDistance(long orbitalDistance) {
 		this.orbitalDistance = orbitalDistance;
 	}
-	public int getDistanceFromSun() {
+	public long getDistanceFromSun() {
 		return distanceFromSun;
 	}
-	public void setDistanceFromSun(int distanceFromSun) {
+	public void setDistanceFromSun(long distanceFromSun) {
 		this.distanceFromSun = distanceFromSun;
 	}
-	public int getSurfaceArea() {
+	public long getSurfaceArea() {
 		return surfaceArea;
 	}
-	public void setSurfaceArea(int surfaceArea) {
+	public void setSurfaceArea(long surfaceArea) {
 		this.surfaceArea = surfaceArea;
 	}
-	public int getLengthOfDay() {
+	public long getLengthOfDay() {
 		return lengthOfDay;
 	}
-	public void setLengthOfDay(int lengthOfDay) {
+	public void setLengthOfDay(long lengthOfDay) {
 		this.lengthOfDay = lengthOfDay;
 	}
-	public int getRadius() {
+	public long getRadius() {
 		return radius;
 	}
-	public void setRadius(int radius) {
+	public void setRadius(long radius) {
 		this.radius = radius;
 	}
-	public int getHasRings() {
+	public boolean isHasRings() {
 		return hasRings;
 	}
-	public void setHasRings(int hasRings) {
+	public void setHasRings(boolean hasRings) {
 		this.hasRings = hasRings;
 	}
 	
-	
-
 }
--- 8Constructor/src/Homework83_PlanetsPrint.java
+++ 8Constructor/src/Homework83_PlanetsPrint.java
@@ -1,4 +1,71 @@
 
 public class Homework83_PlanetsPrint {
+public static void main(String[] args) {
+	
+	Homework83_Planets mercury = new Homework83_Planets();
+	mercury.orbitalDistance = 58000000;
+	mercury.distanceFromSun = 292900000;
+	mercury.surfaceArea = 28880000;
+	mercury.lengthOfDay = 1407;
+	mercury.radius = 1516;
+	mercury.hasRings = false;
+	
+	Homework83_Planets venus = new Homework83_Planets();
+	venus.orbitalDistance = 108000000;
+	venus.distanceFromSun = 67025000;
+	venus.surfaceArea = 177700000;
+	venus.lengthOfDay = 5832;
+	venus.radius = 3760;
+	venus.hasRings = false;
+	
+	Homework83_Planets earth = new Homework83_Planets();
+	earth.orbitalDistance = 149600000;
+	earth.distanceFromSun = 94452000;
+	earth.surfaceArea = 196900000;
+	earth.lengthOfDay = 24;
+	earth.radius = 3958;
+	earth.hasRings = false;
+	
+	Homework83_Planets mars = new Homework83_Planets();
+	mars.orbitalDistance = 347472000;
+	mars.distanceFromSun = 155750000;
+	mars.surfaceArea = 55910000;
+	mars.lengthOfDay = 25;
+	mars.radius = 2106;
+	mars.hasRings = false;
+	
+	Homework83_Planets jupiter = new Homework83_Planets();
+	jupiter.orbitalDistance = ;
+	jupiter.distanceFromSun = 468120000;
+	jupiter.surfaceArea = 23710000000;
+	jupiter.lengthOfDay = 10;
+	jupiter.radius = 43441;
+	jupiter.hasRings = true;
+	
+	Homework83_Planets saturn = new Homework83_Planets();
+	saturn.orbitalDistance = 1427000000;
+	saturn.distanceFromSun = 923510000;
+	saturn.surfaceArea = 16490000000;
+	saturn.lengthOfDay = 11;
+	saturn.radius = 36184;
+	saturn.hasRings = true;
 
+	Homework83_Planets uranus = new Homework83_Planets();
+	uranus.orbitalDistance = 2750000000;
+	uranus.distanceFromSun = 1835100000;
+	uranus.surfaceArea = 3121000000;
+	uranus.lengthOfDay = 17;
+	uranus.radius = 15759;
+	uranus.hasRings = true;
+
+	Homework83_Planets neptune = new Homework83_Planets();
+	mercury.orbitalDistance = 4500000000;
+	mercury.distanceFromSun = 2780800000;
+	mercury.surfaceArea = 2941000000;
+	mercury.lengthOfDay = 16;
+	mercury.radius = 15299;
+	mercury.hasRings = true;
+
+	System.out.println("------------------------------------------------------------");
+	}
 }
